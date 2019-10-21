ba1270638492415ce95a38ae20473a2f104719b8
Alexander Petrenko <gafk555@gmail.com>
2019-10-21 18:40:33 +0400

    add base cleanable entity

---
+++ lib/entities/basecleanableentity.php
@@ -0,0 +1,36 @@
+<?php
+
+// TODO: PHP Docs
+abstract class BaseCleanableEntity
+{
+    protected $searchKeyWord = '';
+
+    protected $elementsId = array();
+
+    /**
+     * BaseCleanableEntity constructor.
+     *
+     * @param string $searchKeyWord
+     */
+    public function __construct(string $searchKeyWord)
+    {
+        $this->searchKeyWord = $searchKeyWord;
+    }
+
+    public function clean() : void
+    {
+        $this->elementsId = $this->findElements();
+        if ($this->elementsId) {
+            $this->removeElements();
+        }
+    }
+
+    protected function getKeyWordFilter() : string
+    {
+        return $this->searchKeyWord . '%';
+    }
+
+    abstract public function findElements() : array;
+
+    abstract protected function removeElements() : void;
+}
