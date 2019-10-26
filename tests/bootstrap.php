1e853003514bc1d026b03c353c2d5eb4637e4502
Alexander Petrenko <gafk555@gmail.com>
2019-10-26 17:04:26 +0400

    add bootstrap for phpunit

---
+++ tests/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+define("NOT_CHECK_PERMISSIONS", true);
+define("NO_AGENT_CHECK", true);
+$GLOBALS["DBType"] = 'mysql';
+$_SERVER["DOCUMENT_ROOT"] = __DIR__ . '/../../../../';
+
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+function initBitrixCore()
+{
+    // manual saving of DB resource
+    global $DB;
+    $app = \Bitrix\Main\Application::getInstance();
+    $con = $app->getConnection();
+    $DB->db_Conn = $con->getResource();
+    // "authorizing" as admin
+    $_SESSION["SESS_AUTH"]["USER_ID"] = 1;
+}
