044d94ced393ad8add8262aa0d23620d85905d7f
Alexander Petrenko <gafk555@gmail.com>
2019-10-26 08:48:40 +0400

    add namespace for classes

--- lib/entities/basecleanableentity.php
+++ lib/entities/basecleanableentity.php
@@ -1,4 +1,4 @@
-<?php
+<?php namespace Petrenko\TestDataCleaner\Entities;
 
 // TODO: PHP Docs
 abstract class BaseCleanableEntity
--- lib/entities/cleanableiblockelement.php
+++ lib/entities/cleanableiblockelement.php
@@ -1,4 +1,4 @@
-<?php
+<?php namespace Petrenko\TestDataCleaner\Entities;
 
 use Bitrix\Iblock\ElementTable;
 
