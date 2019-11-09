920a1055a64d0adcac15f15cb620a28b206f41b3
Alexander Petrenko <gafk555@gmail.com>
2019-11-09 12:06:18 +0400

    add test iblock entity

---
+++ lib/entities/testiblock.php
@@ -0,0 +1,28 @@
+<?php namespace Petrenko\TestDataCleaner\Entities;
+
+use CIBlock;
+use Bitrix\Iblock\IblockTable;
+
+// TODO: PHP Doc
+class TestIblock extends BaseTestEntity
+{
+    public function findElements(): array
+    {
+        $elements = IblockTable::getList(array(
+            'filter' => array(
+                'NAME' => $this->testDataFilter->getFilter(),
+            ),
+            'select' => array('ID', 'NAME'),
+        ))->fetchAll();
+
+        return array_map(function ($el) { return $el['ID']; }, $elements);
+    }
+
+    protected function removeElements(): void
+    {
+        foreach ($this->elementsId as $elementId)
+        {
+            IblockTable::delete($elementId);
+        }
+    }
+}
