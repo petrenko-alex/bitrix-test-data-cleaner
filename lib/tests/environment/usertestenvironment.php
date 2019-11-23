6bd191595e8f7cd69c740ff80c68f8d5dc00de9d
Alexander Petrenko <gafk555@gmail.com>
2019-11-23 19:14:35 +0400

    handle uf fields in user test env

--- lib/tests/environment/usertestenvironment.php
+++ lib/tests/environment/usertestenvironment.php
@@ -1,7 +1,9 @@
 <?php namespace Petrenko\TestDataCleaner\Tests\Environment;
 
 use CUser;
+use CUserTypeEntity;
 use Bitrix\Main\UserTable;
+use Bitrix\Main\UserFieldTable;
 use Petrenko\TestDataCleaner\TestDataFilter;
 
 /**
@@ -13,6 +15,7 @@ class UserTestEnvironment
 
     public static function create(): void
     {
+        static::createUfs();
         static::createUsers();
     }
 
@@ -80,8 +83,80 @@ class UserTestEnvironment
         }
     }
 
+    protected static function createUfs(): void
+    {
+        $commonFields = [
+            'ENTITY_ID' => 'USER',
+            'USER_TYPE_ID' => 'string',
+        ];
+        $ufFields = [
+            array_merge($commonFields, [
+                'FIELD_NAME' => strtoupper('uf_' . static::$prefix . 'uf_1'),
+                'EDIT_FORM_LABEL' => [
+                    'ru' => static::$prefix . 'uf_1',
+                    'en' => '',
+                ],
+            ]),
+            array_merge($commonFields, [
+                'FIELD_NAME' => strtoupper('uf_' . static::$prefix . 'uf_2'),
+                'LIST_COLUMN_LABEL' => [
+                    'ru' => '',
+                    'en' => static::$prefix . 'uf_2',
+                ],
+            ]),
+            array_merge($commonFields, [
+                'FIELD_NAME' => strtoupper('uf_' . static::$prefix . 'uf_3'),
+                'LIST_FILTER_LABEL' => [
+                    'ru' => static::$prefix . 'uf_3',
+                    'en' => static::$prefix . 'uf_3',
+                ],
+            ]),
+            array_merge($commonFields, [
+                'FIELD_NAME' => strtoupper('uf_' . static::$prefix . 'uf_4'),
+                'ERROR_MESSAGE' => [
+                    'ru' => '',
+                    'en' => '',
+                ],
+            ]),
+            array_merge($commonFields, [
+                'FIELD_NAME' => strtoupper('uf_' . static::$prefix . 'uf_5'),
+                'LIST_COLUMN_LABEL' => [
+                    'ru' => TestDataFilter::DEFAULT_FILTER_KEYWORD . static::$prefix . 'uf_5',
+                    'en' => '',
+                ],
+            ]),
+            array_merge($commonFields, [
+                'FIELD_NAME' => strtoupper('uf_' . static::$prefix . 'uf_6'),
+                'LIST_FILTER_LABEL' => [
+                    'ru' => '',
+                    'en' => TestDataFilter::DEFAULT_FILTER_KEYWORD . static::$prefix . 'uf_6',
+                ],
+            ]),
+            array_merge($commonFields, [
+                'FIELD_NAME' => strtoupper('uf_' . static::$prefix . 'uf_7'),
+                'ERROR_MESSAGE' => [
+                    'ru' => static::$prefix . 'uf_7' . TestDataFilter::DEFAULT_FILTER_KEYWORD,
+                    'en' => static::$prefix . 'uf_7' . TestDataFilter::DEFAULT_FILTER_KEYWORD,
+                ],
+            ]),
+            array_merge($commonFields, [
+                'FIELD_NAME' => strtoupper('uf_' . static::$prefix . 'uf_8'),
+                'HELP_MESSAGE' => [
+                    'ru' => static::$prefix . 'uf_7' . TestDataFilter::DEFAULT_FILTER_KEYWORD . 'extra',
+                    'en' => '',
+                ],
+            ]),
+        ];
+
+        $ufField = new CUserTypeEntity();
+        foreach ($ufFields as $fields) {
+            $ufField->Add($fields);
+        }
+    }
+
     public static function remove(): void
     {
+        static::removeUfs();
         static::removeUsers();
     }
 
@@ -94,6 +169,19 @@ class UserTestEnvironment
 
         foreach ($users as $user) {
             CUser::Delete($user['ID']);
+        }
+    }
+
+    protected static function removeUfs(): void
+    {
+        $ufs = UserFieldTable::getList([
+            'filter' => ['FIELD_NAME' => '%' . static::$prefix . '%'],
+            'select' => ['ID'],
+        ])->fetchAll();
+
+        $ufField = new CUserTypeEntity();
+        foreach ($ufs as $uf) {
+            $ufField->Delete($uf['ID']);
         }
     }
 }
